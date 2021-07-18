9b01ff103869e0c1335e139cc557b07483613720
Aman Srivastava <aman11srivastava@gmail.com>
2021-07-18 20:17:37 +0530

    Minor fix in Series component

--- src/Pages/Series/Series.tsx
+++ src/Pages/Series/Series.tsx
@@ -14,14 +14,14 @@ export const Series = () => {
     const [genres, setGenres] = useState<GenreType[]>([]);
     const genreforURL = useGenre(selectedGenres)
 
-    const fetchMovies = async () => {
+    const fetchTVShows = async () => {
         const {data} = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`)
         setContent(data.results);
         setNumberOfPages(data.total_pages);
     }
 
     useEffect(() => {
-        fetchMovies()
+        fetchTVShows()
         // eslint-disable-next-line
     }, [page, genreforURL])
 
